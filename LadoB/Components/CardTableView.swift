829712ab2d29cbadb514429242ce7d0dfb71e4f7
Marcos <marcosraach.1@gmail.com>
2025-05-14 14:46:06 -0300

    feat: card view, tableView and tableViewCell

---
+++ LadoB/Components/CardTableView.swift
@@ -0,0 +1,87 @@
+//
+//  SearchTableView.swift
+//  LadoB
+//
+//  Created by Marcos on 13/05/25.
+//
+
+import UIKit
+
+class CardTableView: UIView {
+    private var albums: [Album] = []
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "default-cell")
+        tableView.register(CardTableViewCell.self, forCellReuseIdentifier: CardTableViewCell.reuseIdentifier)
+        tableView.backgroundColor = .purple1
+        
+        return tableView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        buildContent()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func buildContent() {
+        guard let url = Bundle.main.url(forResource: "mockedData", withExtension: "json"),
+              let data = try? Data(contentsOf: url),
+              let decoded = try? JSONDecoder().decode([Album].self, from: data) else {
+            return
+        }
+        albums = decoded
+    }
+    
+    func updateAlbums(_ albums: [Album]) {
+        self.albums = albums
+        tableView.reloadData()
+    }
+}
+
+extension CardTableView: ViewCodeProtocol {
+    func addSubviews() {
+        addSubview(tableView)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
+
+extension CardTableView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CardTableViewCell.reuseIdentifier, for: indexPath) as? CardTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.config(with: albums[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        return cell
+    }
+}
+
+extension CardTableView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albums.count
+    }
+}
