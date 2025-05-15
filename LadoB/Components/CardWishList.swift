43b270314cfa1cad25ffc635c8704e3df4cc077b
vitor <viktorbruno2006@gmail.com>
2025-05-15 01:20:27 -0300

    feat: JSON albuns decode + component Card for WishList

---
+++ LadoB/Components/CardWishList.swift
@@ -0,0 +1,20 @@
+//
+//  CardWishList.swift
+//  LadoB
+//
+//  Created by Vítor Bruno on 15/05/25.
+//
+
+import UIKit
+
+class CardWishList: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
